602e3b0935fcabced6548f7d387f35cfa6593506
SangJun Lee <66112027+Sangjun-man@users.noreply.github.com>
2023-04-19 01:02:17 +0900

    fix : refresh토큰 페이로드에 잘못된 값 입력된 것 수정 (#12)
    auth Controller에서 잘못된 값으로 엑세스토큰을 발급, guard에서 세팅해준 user값에
    userId가 있는데. user.userId값으로 발급한게 아닌 user.id로 발급

--- src/domain/app/app.module.ts
+++ src/domain/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from '../admin/admin.module';
 import { AuthModule } from '../auth/auth.module';
